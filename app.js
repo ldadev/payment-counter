3801f7e53f71acd02b392eaf96813bb1c929d5ac
David martinez <ldavidme7@gmail.com>
2023-05-12 11:25:49 -0500

    Se modifico el intervalo de los dias

--- app.js
+++ app.js
@@ -24,15 +24,19 @@ const months = [
   const giveaway = document.querySelector('.giveaway');
   const deadline = document.querySelector('.deadline');
   const items = document.querySelectorAll('.deadline-format h4');
-  
+
   let tempDate = new Date();
   let tempYear = tempDate.getFullYear();
   let tempMonth = tempDate.getMonth();
   let tempDay = tempDate.getDate();
-  // months are ZERO index based;
-  const futureDate = new Date(tempYear, tempMonth, tempDay + 20, 11, 30, 0);
+  let change_days = tempDay - 6
+  7
 
-  
+  console.log(change_days)
+  // months are ZERO index based;
+  const futureDate = new Date(tempYear, tempMonth, 20, 11, 30, 0);
+
+
   // let futureDate = new Date(2020, 3, 24, 11, 30, 0);
   
   const year = futureDate.getFullYear();
@@ -47,10 +51,11 @@ const months = [
   giveaway.textContent = `Pagaran el ${weekday}, ${date} de ${month} de ${year} a las ${hours}:${minutes}am`;
   
   const futureTime = futureDate.getTime();
+ 
   function getRemaindingTime() {
     const today = new Date().getTime();
-  
     const t = futureTime - today;
+
     // 1s = 1000ms
     // 1m = 60s
     // 1hr = 60m
@@ -62,6 +67,8 @@ const months = [
     // calculate all values
     let days = t / oneDay;
     days = Math.floor(days);
+   
+  
     let hours = Math.floor((t % oneDay) / oneHour);
     let minutes = Math.floor((t % oneHour) / oneMinute);
     let seconds = Math.floor((t % oneMinute) / 1000);
@@ -84,8 +91,10 @@ const months = [
       clearInterval(countdown);
       deadline.innerHTML = `<h4 class="expired">sorry, this giveaway has expired!</h4>`;
     }
+  
   }
   // countdown;
   let countdown = setInterval(getRemaindingTime, 1000);
   //set initial values
   getRemaindingTime();
+  
